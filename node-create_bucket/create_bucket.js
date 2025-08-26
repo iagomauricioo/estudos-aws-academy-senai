74cb9ad9d75be26cbe841fb3710dba9de3786280
Iago Mauricio <iagomauricio7@gmail.com>
2025-08-26 16:19:01 -0300

    criando bucket s3 com sdk

---
+++ node-create_bucket/create_bucket.js
@@ -0,0 +1,35 @@
+/*
+	Copyright @2019 [Amazon Web Services] [AWS]
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	    http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+const { S3Client, CreateBucketCommand } = require("@aws-sdk/client-s3");
+
+const s3 = new S3Client({
+    region: "us-east-1"
+})
+
+async function createBucket(){
+	try {
+    	var params = {
+            Bucket: "iago", 
+        };
+            
+        const data = await s3.send(new CreateBucketCommand(params));
+        console.log("Bucket criado com sucesso:", data)
+	} catch (e) {
+	    console.error("Error ao criar bucket", e);
+	}
+}
+
+createBucket();
