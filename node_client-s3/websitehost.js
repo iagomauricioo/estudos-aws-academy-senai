78d4e3bf7db16c3cbcacffd244482904b2f80b35
Iago Mauricio <iagomauricio7@gmail.com>
2025-08-27 15:42:41 -0300

    feito a parte de websitehost e trocando nome da pasta

---
+++ node_client-s3/websitehost.js
@@ -0,0 +1,45 @@
+/*
+	Copyright @2019 [Amazon Web Services] [AWS]
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	    http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+import { S3Client, PutBucketWebsiteCommand } from "@aws-sdk/client-s3";
+import fs from "fs";
+
+const s3 = new S3Client({
+    region: "us-east-1"
+})
+
+const BUCKET_NAME = "20250826-iago-catlostandfoundwebsite"
+
+async function makeBucketWebsiteEnabled(){
+    try {
+        const params = {
+            Bucket: BUCKET_NAME,
+            WebsiteConfiguration: {
+	            ErrorDocument: {
+	            	Key: "error.html"
+	            },
+	            IndexDocument: {
+	            	Suffix: "index.html"
+	            }
+            }
+        };
+        const data = await s3.send(new PutBucketWebsiteCommand(params));
+        console.log("Política enviada:", data);
+    } catch (e) {
+        console.log("Erro ao criar política de bucket:", e);
+    }
+};
+
+makeBucketWebsiteEnabled();
